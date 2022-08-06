6716bf1be9250af70d178f2f8c8b740d37e27cf1
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2022-08-05 21:56:01 -0500

    Estructura del Proyecto FREEMOVE

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// Componentes
+import { AboutComponent } from './pages/about/about.component';
+import { CityComponent } from './pages/city/city.component';
+import { HealthComponent } from './pages/health/health.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ServicesComponent } from './pages/services/services.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'ciudad',
+    component: CityComponent,
+  },
+  {
+    path: 'salud',
+    component: HealthComponent,
+  },
+  {
+    path: 'servicios',
+    component: ServicesComponent,
+  },
+  { path: '**', redirectTo: '' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' }),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { CityComponent } from './pages/city/city.component';
+import { HealthComponent } from './pages/health/health.component';
+import { ServicesComponent } from './pages/services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    CityComponent,
+    HealthComponent,
+    ServicesComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
