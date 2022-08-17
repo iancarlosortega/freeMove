2ee4787a2a8767386f3b1987925cda237b36a61e
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2022-08-17 15:42:32 -0500

    Dashboard responsive

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,26 +3,33 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { MaterialModule } from '../material/material.module';
+import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { SharedModule } from '../shared/shared.module';
 
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RoutesComponent } from './routes/routes.component';
 import { IncidentsComponent } from './incidents/incidents.component';
 import { CommunityComponent } from './community/community.component';
 import { ProfileComponent } from './profile/profile.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     DashboardComponent,
-    NavbarComponent,
     RoutesComponent,
     IncidentsComponent,
     CommunityComponent,
     ProfileComponent,
+    NavComponent,
   ],
-  imports: [CommonModule, DashboardRoutingModule, MaterialModule, SharedModule],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    MaterialModule,
+    PrimeNgModule,
+    SharedModule,
+  ],
 })
 export class DashboardModule {}
--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  exports: [InputTextModule],
 })
-export class PrimeNgModule { }
+export class PrimeNgModule {}
