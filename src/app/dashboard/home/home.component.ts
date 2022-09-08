408a82390b05c13e083033dde59c67cdc91f0e3d
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2022-09-08 16:05:17 -0500

    Mostrar las estadíSTsticas de salud en el dashboard

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, switchMap } from 'rxjs';
+import { Route } from 'src/app/interfaces';
+import { RouteService, UserService } from 'src/app/services';
 import SwiperCore, { Autoplay } from 'swiper';
 SwiperCore.use([Autoplay]);
 
@@ -7,7 +10,7 @@ SwiperCore.use([Autoplay]);
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   //TODO: Eliminar estas imágenes
   photos = [
     {
@@ -28,7 +31,78 @@ export class HomeComponent implements OnInit {
     },
   ];
 
-  constructor() {}
+  currentDay = new Date();
+  currentDayRoutes: Route[] = [];
+  currentMonthRoutes: Route[] = [];
+  monthStadistics = {
+    distanceTraveled: 0,
+    burnoutCalories: 0,
+    numberOfRoutes: 0,
+  };
+  dayStadistics = {
+    distanceTraveled: 0,
+    burnoutCalories: 0,
+    numberOfRoutes: 0,
+  };
+  userObs!: Subscription;
 
-  ngOnInit(): void {}
+  constructor(
+    private userService: UserService,
+    private routeService: RouteService
+  ) {}
+
+  ngOnInit(): void {
+    this.userObs = this.userService.user$
+      .pipe(switchMap((user) => this.routeService.getRoutesByUser(user.idUser)))
+      .subscribe((routes) => {
+        // Filtrar rutas del mes actual
+        this.currentMonthRoutes = routes.filter((route) => {
+          const routeDate = new Date(route.startDate);
+          return (
+            routeDate.getMonth() === this.currentDay.getMonth() &&
+            routeDate.getFullYear() === this.currentDay.getFullYear()
+          );
+        });
+
+        // Filtrar rutas del día actual
+        this.currentDayRoutes = routes.filter((route) => {
+          const routeDate = new Date(route.startDate);
+          return (
+            routeDate.getDate() === this.currentDay.getDate() &&
+            routeDate.getMonth() === this.currentDay.getMonth() &&
+            routeDate.getFullYear() === this.currentDay.getFullYear()
+          );
+        });
+
+        // Obtener las estadísticas del mes
+        this.monthStadistics.distanceTraveled = this.currentMonthRoutes.reduce(
+          (prev, current) => current.distance + prev,
+          0
+        );
+
+        this.monthStadistics.burnoutCalories = this.currentMonthRoutes.reduce(
+          (prev, current) => current.burnoutCalories + prev,
+          0
+        );
+
+        this.monthStadistics.numberOfRoutes = this.currentMonthRoutes.length;
+
+        // Obtener las estadísticas del día
+        this.dayStadistics.distanceTraveled = this.currentDayRoutes.reduce(
+          (prev, current) => current.distance + prev,
+          0
+        );
+
+        this.dayStadistics.burnoutCalories = this.currentDayRoutes.reduce(
+          (prev, current) => current.burnoutCalories + prev,
+          0
+        );
+
+        this.dayStadistics.numberOfRoutes = this.currentDayRoutes.length;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.userObs.unsubscribe();
+  }
 }
