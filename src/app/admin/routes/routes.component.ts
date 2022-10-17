7655ebfbbab58d38748d04dcda03253183ebf27a
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2022-10-16 20:24:12 -0500

    Cambiar el mapa de calor de los incidentes recurrentes por marcadores

--- src/app/admin/routes/routes.component.ts
+++ src/app/admin/routes/routes.component.ts
@@ -1,7 +1,14 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { Map } from 'mapbox-gl';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
+import { Map, Marker, Popup } from 'mapbox-gl';
 import moment from 'moment';
-import { Route } from 'src/app/interfaces';
+import { switchMap, tap } from 'rxjs';
+import { Incident, Route } from 'src/app/interfaces';
 import { IncidentService, RouteService } from 'src/app/services';
 import { mapRoute } from 'src/app/utils';
 
@@ -16,6 +23,7 @@ export class RoutesComponent implements AfterViewInit {
   routeMap!: Map;
   incidentMap!: Map;
   routes: Route[] = [];
+  incidents: Incident[] = [];
   isLoading: boolean = true;
   currentHours: any[] = [];
   firstRange: number = 0;
@@ -24,19 +32,26 @@ export class RoutesComponent implements AfterViewInit {
   fourthRange: number = 0;
 
   constructor(
+    private renderer: Renderer2,
     private routeService: RouteService,
     private incidentService: IncidentService
-  ) { }
+  ) {}
 
   ngAfterViewInit(): void {
-    this.routeService.getRoutes().subscribe((routes) => {
-      this.routes = routes.map((route: Route) => mapRoute(route));
-      this.isLoading = false;
-      this.getRangeCounters();
-      this.getCurrentHours();
-      this.getRecurrentRoutes();
-      this.getRecurrentIncidents();
-    });
+    this.routeService
+      .getRoutes()
+      .pipe(
+        tap((routes) => (this.routes = routes.map((route) => mapRoute(route)))),
+        switchMap(() => this.incidentService.getIncidents())
+      )
+      .subscribe((incidents) => {
+        this.incidents = incidents;
+        this.isLoading = false;
+        this.getRangeCounters();
+        this.getCurrentHours();
+        this.getRecurrentRoutes();
+        this.getRecurrentIncidents();
+      });
   }
 
   getRecurrentRoutes() {
@@ -44,13 +59,11 @@ export class RoutesComponent implements AfterViewInit {
       container: this.mapRoutes.nativeElement,
       style: 'mapbox://styles/mapbox/satellite-streets-v11',
       center: [-79.207599, -4.001713],
-      // center: this.routes[0].startPosition,
       zoom: 12,
       bearing: 0,
     });
 
     this.routeMap.on('load', () => {
-      // this.add3D();
       this.routes.forEach((route, index) => {
         this.routeMap.addSource('route' + index, {
           type: 'geojson',
@@ -127,90 +140,30 @@ export class RoutesComponent implements AfterViewInit {
   }
 
   getRecurrentIncidents() {
-    this.incidentService.getIncidents().subscribe((incidents) => {
-      this.incidentMap = new Map({
-        container: this.mapIncidents.nativeElement,
-        style: 'mapbox://styles/mapbox/satellite-streets-v11',
-        center: [-79.207599, -4.001713],
-        // center: this.routes[0].startPosition,
-        zoom: 12,
-        bearing: 0,
-      });
+    this.incidentMap = new Map({
+      container: this.mapIncidents.nativeElement,
+      style: 'mapbox://styles/mapbox/satellite-streets-v11',
+      center: [-79.207599, -4.001713],
+      zoom: 12,
+      bearing: 0,
+    });
 
-      this.incidentMap.on('load', () => {
-        // this.add3D();
-        incidents.forEach((incident, index) => {
-          this.incidentMap.addSource('incident' + index, {
-            type: 'geojson',
-            data: {
-              type: 'Feature',
-              properties: {},
-              geometry: {
-                type: 'LineString',
-                coordinates: [incident.position],
-              },
-            },
-          });
+    this.incidentMap.on('load', () => {
+      this.incidents.forEach((incident) => {
+        const popup = new Popup({ offset: 25 }).setHTML(
+          `<h5>${incident.title}</h5>
+        <p>${incident.description}</p>
+        <a class="primary-button" href="/dashboard/incidentes/${incident.idIncident}">Ver Más</a>`
+        );
 
-          this.incidentMap.addLayer(
-            {
-              id: 'incidentheat' + index,
-              type: 'heatmap',
-              source: 'incident' + index,
-              maxzoom: 24,
-              paint: {
-                // increase weight as diameter breast height increases
-                'heatmap-weight': {
-                  property: 'dbh',
-                  type: 'exponential',
-                  stops: [
-                    [1, 0],
-                    [62, 1],
-                  ],
-                },
-                // increase intensity as zoom level increases
-                'heatmap-intensity': {
-                  stops: [
-                    [11, 1],
-                    [15, 3],
-                  ],
-                },
-                // assign color values be applied to points depending on their density
-                'heatmap-color': [
-                  'interpolate',
-                  ['linear'],
-                  ['heatmap-density'],
-                  0,
-                  'rgba(236,222,239,0)',
-                  0.2,
-                  'rgb(208,209,230)',
-                  0.4,
-                  'rgb(166,189,219)',
-                  0.6,
-                  'rgb(103,169,207)',
-                  0.8,
-                  'rgb(28,144,153)',
-                ],
-                // increase radius as zoom increases
-                'heatmap-radius': {
-                  stops: [
-                    [11, 15],
-                    [15, 20],
-                  ],
-                },
-                // decrease opacity to transition into the circle layer
-                'heatmap-opacity': {
-                  default: 1,
-                  stops: [
-                    [14, 1],
-                    [15, 0],
-                  ],
-                },
-              },
-            },
-            'waterway-label'
-          );
-        });
+        const element = this.renderer.createElement('div');
+        this.renderer.addClass(element, 'marker');
+        this.renderer.addClass(element, 'incident-marker');
+
+        new Marker(element)
+          .setLngLat(incident.position)
+          .setPopup(popup)
+          .addTo(this.incidentMap);
       });
     });
   }
@@ -251,34 +204,5 @@ export class RoutesComponent implements AfterViewInit {
     resultArray.sort((a, b) => b.count - a.count);
     // Get the first 3 elements
     this.currentHours = resultArray.slice(0, 5);
-  }
-
-  add3D() {
-    // add map 3d terrain and sky layer and fog
-    // Add some fog in the background
-    this.routeMap.setFog({
-      range: [0.5, 10],
-      color: 'white',
-      'horizon-blend': 0.2,
-    });
-
-    // Add a sky layer over the horizon
-    this.routeMap.addLayer({
-      id: 'sky',
-      type: 'sky',
-      paint: {
-        'sky-type': 'atmosphere',
-        'sky-atmosphere-color': 'rgba(85, 151, 210, 0.5)',
-      },
-    });
-
-    // Add terrain source, with slight exaggeration
-    this.routeMap.addSource('mapbox-dem', {
-      type: 'raster-dem',
-      url: 'mapbox://mapbox.terrain-rgb',
-      tileSize: 512,
-      maxzoom: 14,
-    });
-    this.routeMap.setTerrain({ source: 'mapbox-dem', exaggeration: 1 });
   }
 }
