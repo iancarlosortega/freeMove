164e9ce2dc178ad78d6f2242f22cd043976f6f89
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2022-11-03 10:24:35 -0500

    Fix: GeolocationService

--- src/app/services/geolocation.service.ts
+++ src/app/services/geolocation.service.ts
@@ -1,15 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Subject, BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GeolocationService {
-  private reactiveDeviceLocation$: Subject<Object>;
-
-  constructor() {
-    this.reactiveDeviceLocation$ = new BehaviorSubject<Object>({});
-  }
+  constructor() {}
 
   getUserLocation(): Promise<[number, number]> {
     return new Promise((resolve, reject) => {
@@ -28,29 +23,5 @@ export class GeolocationService {
         }
       );
     });
-  }
-
-  getLocation(): Observable<Object> {
-    const opts = {
-      enableHighAccuracy: true,
-      maximumAge: 60000,
-      timeout: 30000,
-    };
-    const watchId = navigator.geolocation.watchPosition(
-      (position) => {
-        const currentLocation = {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        };
-        console.log(currentLocation);
-        this.reactiveDeviceLocation$.next(currentLocation);
-      },
-      (err) => {
-        console.log(err);
-      },
-      opts
-    );
-    console.log(watchId);
-    return this.reactiveDeviceLocation$;
   }
 }
