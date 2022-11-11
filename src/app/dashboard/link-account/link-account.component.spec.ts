eb7bc4b5c46e900a67853730610ec05c2f8d6973
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2022-11-11 15:20:25 -0500

    LinkAccountComponent test

--- src/app/dashboard/link-account/link-account.component.spec.ts
+++ src/app/dashboard/link-account/link-account.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, ReactiveFormsModule } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Observable, of } from 'rxjs';
@@ -40,6 +40,7 @@ class MatDialogStub {}
 describe('LinkAccountComponent', () => {
   let component: LinkAccountComponent;
   let fixture: ComponentFixture<LinkAccountComponent>;
+  let linkedEmailField: AbstractControl;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -57,7 +58,59 @@ describe('LinkAccountComponent', () => {
     fixture.detectChanges();
   });
 
+  beforeEach(() => {
+    linkedEmailField = component.linkAccountForm.controls['linkedEmail'];
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a form with linkedEmail field', () => {
+    linkedEmailField.enable();
+    expect(component.linkAccountForm.contains('linkedEmail')).toBeTruthy();
+  });
+
+  it('should linkedEmail field be required', () => {
+    linkedEmailField.enable();
+    linkedEmailField.setValue('');
+    expect(linkedEmailField.valid).toBeFalsy();
+  });
+
+  it('should linkedEmail field have to be a valid email', () => {
+    linkedEmailField.enable();
+    linkedEmailField.setValue('iancarlosortegaleon');
+    expect(linkedEmailField.valid).toBeFalsy();
+
+    linkedEmailField.setValue('iancarlosortegaleon@gmail.com');
+    expect(linkedEmailField.valid).toBeTruthy();
+  });
+
+  it('should return false if input is invalid', () => {
+    linkedEmailField.enable();
+    linkedEmailField?.setValue('');
+    linkedEmailField?.markAsTouched();
+    expect(component.invalidInput('linkedEmail')).toBeTruthy();
+  });
+
+  it('should return true if input is valid', () => {
+    linkedEmailField.enable();
+    linkedEmailField?.setValue('iancarlosortegaleon@gmail.com');
+    linkedEmailField?.markAsTouched();
+    expect(component.invalidInput('linkedEmail')).toBeFalsy();
+  });
+
+  it('should linkedEmail field have to be disabled when notification status is pending', () => {
+    expect(linkedEmailField.disabled).toBeTruthy();
+  });
+
+  it('should button have text "Invitación enviada" when notification status is pending', () => {
+    const button = fixture.nativeElement.querySelector('.primary-button');
+    expect(button.textContent).toContain('Invitación enviada');
+  });
+
+  it('should button be disabled when notification status is pending', () => {
+    const button = fixture.nativeElement.querySelector('.primary-button');
+    expect(button.disabled).toBeTruthy();
   });
 });
