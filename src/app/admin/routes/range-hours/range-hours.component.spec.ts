e2052e7ecfe1bb4db822a58ec6aa514d7fa756c9
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2022-11-13 20:58:24 -0500

    RangeHoursComponent test

--- src/app/admin/routes/range-hours/range-hours.component.spec.ts
+++ src/app/admin/routes/range-hours/range-hours.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import moment from 'moment';
 
 import { RangeHoursComponent } from './range-hours.component';
 
@@ -8,9 +9,8 @@ describe('RangeHoursComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RangeHoursComponent ]
-    })
-    .compileComponents();
+      declarations: [RangeHoursComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RangeHoursComponent);
     component = fixture.componentInstance;
@@ -19,5 +19,124 @@ describe('RangeHoursComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should calculate the different hour ranges from the routes', () => {
+    component.routes = [
+      {
+        idRoute: '1',
+        idUser: '1',
+        startDate: new Date('2021-05-01T03:00:00'),
+        endDate: new Date('2021-05-01T03:00:00'),
+        distance: 0,
+        name: 'Ruta 1',
+        burnoutCalories: 0,
+        coordinates: [],
+        calification: 0,
+        difficulty: 'Fácil',
+        endPosition: [0, 0],
+        keywords: [],
+        photos: [],
+        startPosition: [0, 0],
+        timeElapsed: 0,
+        type: 'Ciclista',
+        velocityAvg: 0,
+        activityType: 'Ciclista',
+        city: 'Loja',
+      },
+    ];
+    component.ngOnInit();
+    expect(component.firstRange).toBe(1);
+    expect(component.secondRange).toBe(0);
+    expect(component.thirdRange).toBe(0);
+    expect(component.fourthRange).toBe(0);
+
+    component.routes = [
+      {
+        idRoute: '1',
+        idUser: '1',
+        startDate: new Date('2021-05-01T08:00:00'),
+        endDate: new Date('2021-05-01T08:00:00'),
+        distance: 0,
+        name: 'Ruta 1',
+        burnoutCalories: 0,
+        coordinates: [],
+        calification: 0,
+        difficulty: 'Fácil',
+        endPosition: [0, 0],
+        keywords: [],
+        photos: [],
+        startPosition: [0, 0],
+        timeElapsed: 0,
+        type: 'Ciclista',
+        velocityAvg: 0,
+        activityType: 'Ciclista',
+        city: 'Loja',
+      },
+    ];
+    component.ngOnInit();
+    expect(component.firstRange).toBe(1);
+    expect(component.secondRange).toBe(1);
+    expect(component.thirdRange).toBe(0);
+    expect(component.fourthRange).toBe(0);
+
+    component.routes = [
+      {
+        idRoute: '1',
+        idUser: '1',
+        startDate: new Date('2021-05-01T13:00:00'),
+        endDate: new Date('2021-05-01T13:00:00'),
+        distance: 0,
+        name: 'Ruta 1',
+        burnoutCalories: 0,
+        coordinates: [],
+        calification: 0,
+        difficulty: 'Fácil',
+        endPosition: [0, 0],
+        keywords: [],
+        photos: [],
+        startPosition: [0, 0],
+        timeElapsed: 0,
+        type: 'Ciclista',
+        velocityAvg: 0,
+        activityType: 'Ciclista',
+        city: 'Loja',
+      },
+    ];
+
+    component.ngOnInit();
+    expect(component.firstRange).toBe(1);
+    expect(component.secondRange).toBe(1);
+    expect(component.thirdRange).toBe(1);
+    expect(component.fourthRange).toBe(0);
+
+    component.routes = [
+      {
+        idRoute: '1',
+        idUser: '1',
+        startDate: new Date('2021-05-01T18:00:00'),
+        endDate: new Date('2021-05-01T18:00:00'),
+        distance: 0,
+        name: 'Ruta 1',
+        burnoutCalories: 0,
+        coordinates: [],
+        calification: 0,
+        difficulty: 'Fácil',
+        endPosition: [0, 0],
+        keywords: [],
+        photos: [],
+        startPosition: [0, 0],
+        timeElapsed: 0,
+        type: 'Ciclista',
+        velocityAvg: 0,
+        activityType: 'Ciclista',
+        city: 'Loja',
+      },
+    ];
+    component.ngOnInit();
+    expect(component.firstRange).toBe(1);
+    expect(component.secondRange).toBe(1);
+    expect(component.thirdRange).toBe(1);
+    expect(component.fourthRange).toBe(1);
   });
 });
