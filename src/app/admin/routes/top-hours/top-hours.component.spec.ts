241df15897b1ce08b469773ab05d7e25aa40e111
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2022-11-13 21:08:09 -0500

    TopHoursComponent test

--- src/app/admin/routes/top-hours/top-hours.component.spec.ts
+++ src/app/admin/routes/top-hours/top-hours.component.spec.ts
@@ -8,9 +8,8 @@ describe('TopHoursComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TopHoursComponent ]
-    })
-    .compileComponents();
+      declarations: [TopHoursComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TopHoursComponent);
     component = fixture.componentInstance;
@@ -19,5 +18,35 @@ describe('TopHoursComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should calculate the top hours from the routes', () => {
+    component.routes = [
+      {
+        idRoute: '1',
+        idUser: '1',
+        startDate: new Date('2021-05-01T03:00:00'),
+        endDate: new Date('2021-05-01T03:00:00'),
+        distance: 0,
+        name: 'Ruta 1',
+        burnoutCalories: 0,
+        coordinates: [],
+        calification: 0,
+        difficulty: 'Fácil',
+        endPosition: [0, 0],
+        keywords: [],
+        photos: [],
+        startPosition: [0, 0],
+        timeElapsed: 0,
+        type: 'Ciclista',
+        velocityAvg: 0,
+        activityType: 'Ciclista',
+        city: 'Loja',
+      },
+    ];
+    component.ngOnInit();
+    expect(component.currentHours.length).toEqual(1);
+    expect(component.currentHours[0].hour).toEqual('3');
+    expect(component.currentHours[0].count).toEqual(1);
   });
 });
