8c2fb19c6612dfb151bd1ac420b2d10a26c33988
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2022-11-28 10:35:05 -0500

    Arreglar errores cuando existem rutas vacías

--- src/app/utils/mapRoute.ts
+++ src/app/utils/mapRoute.ts
@@ -7,18 +7,22 @@ export const mapRoute = (route: Route) => {
     const routeArray = route.split(',');
     return [parseFloat(routeArray[1]), parseFloat(routeArray[0])];
   });
-  const startPositionArray = (route.startPosition as string).split(',');
-  route.startPosition = [
-    parseFloat(startPositionArray[1]),
-    parseFloat(startPositionArray[0]),
-  ];
-  const endPositionArray = (route.endPosition as string).split(',');
-  route.endPosition = [
-    parseFloat(endPositionArray[1]),
-    parseFloat(endPositionArray[0]),
-  ];
-  route.startDate = route.startDate.toDate();
-  route.endDate = route.endDate.toDate();
+  const startPositionArray = (route.startPosition as string)?.split(',');
+  if (startPositionArray) {
+    route.startPosition = [
+      parseFloat(startPositionArray[1]),
+      parseFloat(startPositionArray[0]),
+    ];
+  }
+  const endPositionArray = (route.endPosition as string)?.split(',');
+  if (endPositionArray) {
+    route.endPosition = [
+      parseFloat(endPositionArray[1]),
+      parseFloat(endPositionArray[0]),
+    ];
+  }
+  route.startDate = route.startDate?.toDate();
+  route.endDate = route.endDate?.toDate();
   const duration = moment.duration(route.timeElapsed, 'milliseconds');
   route.timeElapsed = Math.round(duration.asMinutes());
   return route;
